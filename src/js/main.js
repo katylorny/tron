160468387c87cc72c7429cfaabdfd97846ab2bae
katylorny <katy_lorny@mail.ru>
2020-09-02 16:32:00 +0300

    окно уведомлений

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,21 @@
-import $ from '../local_modules/jquery/dist/jquery.min'
+import $ from 'jquery'
+import "popper.js"
+import "bootstrap"
+//
+// $(function () {
+//     $(`[data-toggle="popover"]`).popover()
+// })
 
 $(document).ready(() => {
     // eslint-disable-next-line no-console
     console.log(`document ready`)
 })
+
+$(`#modalNotifications`).modal({
+    backdrop: false,
+    // show: false
+})
+
+// $(`#modalNotifications`).on(`shown.bs.modal`, function () {
+//     $(`.modal-backdrop`).addClass(`modal-backdrop--transparent`)
+// })
